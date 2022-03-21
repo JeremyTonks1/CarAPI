637cc5472b074d0bd7f47e42410cd7ebe9f2c269
JeremyTonks1 <jeremyTonks1@outlook.com>
2022-03-21 16:22:22 +1300

    Moving Code.
    Moving the business logic away from the front-end to be processed on the server-side.

--- js/Cars.js
+++ js/Cars.js
@@ -1,259 +1,64 @@
-//Car API
+var table
 
-///Car storage object.
-const CarStorage = {
-  carID: 0,
-  carArray: [],
-  AddCar: function(carYear, carBrand, carModel, carEngine){
-    //Creates a new car object, add it to the Car Storage.
-    const _car = new Car(this.carID, carYear, carBrand, carModel, carEngine);
-    this.carArray.push(_car);
-    //Updates the unquie identifier.
-    this.carID += 1;
+//Car API
+const CarAPI = {
+  selectedID: -1,
+  AddCar: function(){
+    $.post("/Add?Year=" + $("#addYear").val() +
+    "&Brand=" + $("#addBrand").val() +
+    "&Model=" + $("#addModel").val() +
+    "&Engine=" + $("#addEngine").val());
   },
-  UpdateCar: function(carID, carYear, carBrand, carModel, carEngine){
+  UpdateCar: function(){
     //Finds the cars index value using the car ID.
-    let index = this.FindCar(carID);
-    //Updates the details of this car object.
-    this.carArray[index].year = carYear;
-    this.carArray[index].brand = carBrand;
-    this.carArray[index].model = carModel;
-    this.carArray[index].engineType = carEngine;
+    $.post("/Update?ID=" + this.selectedID +
+    "&Year=" + $("#updateYear").val() +
+    "&Brand=" + $("#updateBrand").val() +
+    "&Model=" + $("#updateModel").val() +
+    "&Engine=" + $("#updateEngine").val());
   },
   DeleteCar: function(carID){
     //Finds the cars index value using the car ID.
-    let index = this.FindCar(carID);
-    //Deletes this car object and then removes it from the array.
-    delete this.carArray[index];
-    this.carArray.splice(index, 1);
-  },
-  FindCar: function(carID){
-    //Goes through each item in the car storage array.
-    for(let i = 0; i < this.carArray.length; i += 1){
-      let _car = this.carArray[i];
-      //Finds the index of the car with this ID.
-      if( _car.ID == carID){
-          return i;
-      }
-    }
+    $.post("/Delete?ID=" + this.selectedID);
+    this.SearchCars('','','')
   },
   //Finds Car Entries.
   SearchCars: function(carID,carYear,carBrand){
-    let results = [];
-    //Goes through each item in the car storage array.
-    for(let i = 0; i < this.carArray.length; i += 1){
-      let _car = this.carArray[i];
-      //Searches for car objects that matches the ID, year, and brand searched.
-      if( _car.ID == carID || carID == '' ){
-        if( _car.year == carYear || carYear == '' ){
-          if( _car.brand.toLowerCase() == carBrand.toLowerCase() || carBrand.toLowerCase() == '' ){
-            results.push(this.carArray[i]);
-          }
-        }
+    $.get("/data?ID=", function(data, status){
+      //Draw Table
+      table.clear().draw()
+      for(let i = 0; i < data.length; i ++){
+        table.row.add(data[i])
       }
-    }
-    //Returns an array of the results that matched the search request.
-    return results;
-  },
-  ImportCar: function(carID, carYear, carBrand, carModel, carEngine){
-    //Creates a new car object, add it to the Car Storage.
-    const _car = new Car(carID, carYear, carBrand, carModel, carEngine);
-    this.carArray.push(_car);
-  },
-}
-//Car Class
-class Car{
-    constructor(carID, carYear, carBrand, carModel, carEngine){
-      this.ID = carID;
-      this.year = carYear;
-      this.brand = carBrand;
-      this.model = carModel;
-      this.engineType = carEngine;
-    }
-}
 
-
-
-
-
-//Debugging !!!!!!Remove in Final Release!!!!!!!!!!
-/*
-//npm install prompt-sync
-//const ps = require('prompt-sync');
-const prompt = ps();
-
-// String Formating Function
-String.prototype.format = function () {
-  // store arguments in an array
-  var args = arguments;
-  // use replace to iterate over the string
-  // select the match and check if related argument is present
-  // if yes, replace the match with the argument
-  return this.replace(/{([0-9]+)}/g, function (match, index) {
-    // check if the argument is present
-    return typeof args[index] == 'undefined' ? match : args[index];
-  });
-};
-
-//Setup up some data for the debug tool.
-CarStorage.AddCar("2017","Honda","Jazz","Petrol");
-CarStorage.AddCar("2004","Nissan","Maxima","Petrol");
-CarStorage.AddCar("2011","Ford","Mondeo","Diesel");
-CarStorage.AddCar("2013","Holden","Captiva","Diesel");
-CarStorage.AddCar("2006","Honda","Odyssey","Petrol");
-CarStorage.AddCar("2006","Porsche","Cayenne","Petrol");
-CarStorage.AddCar("2018","Honda","Jazz","Petrol");
-
-//Display this to the user when they load the program on Node.
-console.log("Car API Debugging Tool: \nType \"help\" to see all the " +
-  "commands, and type \"exit\" to close the program type.")
-while (true){
-  let input = prompt(">> ").toLowerCase();
-  //User wants to see all the available commands.
-  if(input == 'help'){
-    console.log("\n\"add\" - adds a car entry.")
-    console.log("\"update\" - updates a car entry.")
-    console.log("\"delete\" - deletes a car entry.")
-    console.log("\"search\" - searches for car entries.")
-    console.log("\"exit\" - closes the program.\n")
-  }
-  //User wants to add a car entry.
-  else if (input == 'add'){
-    console.log("\nAdd a car by typing in the cars <year>, <brand>," +
-    " <model>, <engine type>. \nFor example: \"2017, Honda, Jazz, Petrol\".")
-    let inputAdd = prompt(">> ").replace(/\s+/g, '');
-    let _result = inputAdd.split(",")
-    //Has the user put in the required request?
-    if(_result.length == 4){
-      CarStorage.AddCar(_result[0],_result[1],_result[2],_result[3]);
-      //Success message.
-      console.log("This car has been added.\n")
-    } else{
-      //Error message.
-      console.log("The input requires a <year>, <brand>, <model>," +
-      " <engine type>.")
-      console.log("make sure there are commas inbetween.\n")
-    }
-  }
-  //User wants to update a car entry.
-  else if (input == 'update'){
-    console.log("\nType in the ID of the car you want to change."+
-  "type in what you want to change <ID>, <year>, <brand>, <model>," +
-  "<engine type>.\nFor example: \"0, 2017, Honda, Jazz, Petrol\".")
-    let inputAdd = prompt(">> ").replace(/\s+/g, '');
-    let _result = inputAdd.split(",")
-    //Has the user put in the required request?
-    if(_result.length == 5){
-      CarStorage.UpdateCar(_result[0],_result[1],_result[2],_result[3],_result[4]);
-      //Success message.
-      console.log("This car has been updated.\n")
-    } else{
-      //Error message.
-      console.log("The input requires an <ID>, <year>, <brand>, <model>," +
-      " <engine type>.")
-      console.log("make sure there are commas inbetween.\n")
-    }
-  }
-  //User wants to delete an entry.
-  else if (input == 'delete'){
-    console.log("\nType in the ID of the car you want to delete.")
-    let inputID = prompt(">> ").replace(/\s+/g, '');
-    //Is this not an existing car that is able to be deleted?
-    if(CarStorage.FindCar(inputID) == undefined){
-      //Error message
-      console.log("car " + inputID + " cannot be found.\n")
-    } else{
-      //Success message.
-      CarStorage.DeleteCar(inputID);
-      console.log("car " + inputID + " has been removed.\n")
-    }
-  }
-  //User wants to find cars.
-  else if (input == 'search'){
-    console.log("\nAdd in the cars <id>, <year>, <brand> " +
-    "to search for. \nFor example: \"0, 2017, Honda\".")
-    let inputAdd = prompt(">> ").replace(/\s+/g, '');
-    let _result = inputAdd.split(",")
-    let _searchResults = [];
-    //Has the User put in the required request?
-    if(_result.length == 3){
-      _searchResults = CarStorage.SearchCars(_result[0],_result[1],_result[2]);
-    } else{
-      _searchResults = CarStorage.SearchCars('','','');
-    }
-    //Format and display the results to the user.
-    for(let i = 0; i < _searchResults.length; i += 1){
-      let _carID = _searchResults[i].ID;
-      let _carYear = _searchResults[i].year;
-      let _carBrand = _searchResults[i].brand;
-      let _carModel = _searchResults[i].model;
-      let _carEngine = _searchResults[i].engineType;
-      let sentence = ("ID: {0}, Year: {1}, Brand: {2}," +
-      " Model: {3}, Engine: {4}.").format(_carID, _carYear, _carBrand,
-      _carModel, _carEngine)
-      console.log(sentence)
-    }
-    //Add some space inbetween.
-    console.log("")
-  }
-  //Exit the program.
-  else if (input == 'exit'){
-    return;
+      table.draw()
+    });
   }
 }
-*/
 
 $(document).ready( function () {
 
-  let row = ''
-  let table = document.getElementById('carContent')
-
-  $.ajax({
-    url: 'xml/Cars.xml',
-    dataType: 'xml',
-    type: "GET",
-    success: function(response){
-      $(response).find("Car").each(function(){
-        let _carID = $(this).find("ID").text();
-        let _carYear = $(this).find("Year").text();
-        let _carBrand = $(this).find("Brand").text();
-        let _carModel = $(this).find("Model").text();
-        let _carEngine = $(this).find("EngineType").text();
-
-        CarStorage.ImportCar(_carID, _carYear, _carBrand, _carModel, _carEngine);
-        row += createString(_carID,_carYear,_carBrand,_carModel,_carEngine)
-
-      });
-      table.innerHTML = row
-
-      //Constructs the Table
-      $('#example').DataTable({
-        ordering: false,
-        searching: false,
-        lengthMenu: [[5,10,20],[5,10,20]]
-      });
-    },
-    error: function(){
-
-    }
+  table = $('#example').DataTable({
+    ordering: false,
+    searching: false,
+    lengthMenu: [[5,10,20],[5,10,20]]
   });
 
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+  //Each Row Function
+  $('#carContent').on('click', 'tr', function() {
+    $('#myModal2').modal('show');
+    //$('#deleteButton').attr('onclick','CarAPI.DeleteCar(' +
+    //  table.row( this ).data()[0]) +')');
 
-  $('#myUpdateModal').on('shown.bs.modal2', function () {
-    $('#myInput').trigger('focus')
-  })
+    $('#updateTitle').html('Car ' + table.row( this ).data()[0])
+    $('#updateYear').val(table.row( this ).data()[1]);
+    $('#updateBrand').val(table.row( this ).data()[2]);
+    $('#updateModel').val(table.row( this ).data()[3]);
+    $('#updateEngine').val(table.row( this ).data()[4]);
+
+    //This is being referenced from the button
+    CarAPI.selectedID = table.row(this).data()[0];
+  });
+
+  CarAPI.SearchCars('','','')
 });
-
-function createString(_carID, _carYear, _carBrand, _carModel, _carEngine){
-  let row = "<tr data-bs-toggle=\"modal2\" data-bs-target=\"#myUpdateModal\">" +
-            "<th scope=\"row\">" + _carID + "</th>" +
-            "<td>" + _carYear + "</td>" +
-            "<td>" + _carBrand + "</td>" +
-            "<td>" + _carModel + "</td>" +
-            "<td>" + _carEngine + "</td>" +
-          "</tr>"
-  return row
-}
--- server.js
+++ server.js
@@ -1,15 +1,125 @@
+
+const fs = require('fs');
+const {parseString, Builder} = require('xml2js');
+
+//Setup the Server
 const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
+//Home page.
 app.get('', (request, response) => {
   response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//HTTP Request for XML data.
+app.post('/add', (request, response) =>{
+  fs.readFile('xml/Cars.xml',  function(err, xml){
+    parseString(xml, function(err, data) {
+      var _car = {
+        ID: data.Info.IDCounter,
+        Year: request.query.Year,
+        Brand: request.query.Brand,
+        Model: request.query.Model,
+        EngineType: request.query.Engine,
+      }
+
+      data.Info.CarStorage[0].Car.push(_car)
+      data.Info.IDCounter = parseInt(data.Info.IDCounter) + 1
+
+      //Convert the JS Object to XML
+      const builder = new Builder();
+      const _xml = builder.buildObject(data);
+      fs.writeFile('xml/Cars.xml', _xml, function (err, file){
+        if(err) throw err;
+      })
+    })
+  })
 })
 
+//HTTP Request for XML data.
+app.post('/update', (request, response) =>{
+  fs.readFile('xml/Cars.xml',  function(err, xml){
+    parseString(xml, function(err, data) {
+
+      //Convert each Car into a Table Row.
+      for(let i = 0; i < data.Info.CarStorage[0].Car.length; i ++){
+        //Searching
+        if(data.Info.CarStorage[0].Car[i].ID == request.query.ID){
+
+          data.Info.CarStorage[0].Car[i].Year = request.query.Year
+          data.Info.CarStorage[0].Car[i].Brand = request.query.Brand
+          data.Info.CarStorage[0].Car[i].Model = request.query.Model
+          data.Info.CarStorage[0].Car[i].EngineType = request.query.Engine
+
+          //Convert the JS Object to XML
+          const builder = new Builder();
+          const _xml = builder.buildObject(data);
+          fs.writeFile('xml/Cars.xml', _xml, function (err, file){
+            if(err) throw err;
+          })
+
+        }
+      }
+    })
+  })
+})
+
+//HTTP Request for XML data.
+app.post('/delete', (request, response) =>{
+  fs.readFile('xml/Cars.xml',  function(err, xml){
+
+    parseString(xml, function(err, data) {
+      //Convert each Car into a Table Row.
+      for(let i = 0; i < data.Info.CarStorage[0].Car.length; i ++){
+        //Searching
+        if(data.Info.CarStorage[0].Car[i].ID == request.query.ID){
+          data.Info.CarStorage[0].Car.splice(i, 1)
+
+          //Convert the JS Object to XML
+          const builder = new Builder();
+          const _xml = builder.buildObject(data);
+          fs.writeFile('xml/Cars.xml', _xml, function (err, file){
+            if(err) throw err;
+          })
+        }
+      }
+    })
+  })
+})
+
+//HTTP Request for XML data.
+app.get('/data', (request, response) =>{
+  let row = [];
+
+  fs.readFile('xml/Cars.xml',  function(err, _xml){
+    parseString(_xml, function(err, data) {
+      //Convert each Car into a Table Row.
+      for(let i = 0; i < data.Info.CarStorage[0].Car.length; i ++){
+        let _carID = data.Info.CarStorage[0].Car[i].ID[0]
+        let _carYear = data.Info.CarStorage[0].Car[i].Year[0]
+        let _carBrand = data.Info.CarStorage[0].Car[i].Brand[0]
+        let _carModel = data.Info.CarStorage[0].Car[i].Model[0]
+        let _carEngine = data.Info.CarStorage[0].Car[i].EngineType[0]
+        //Searching
+        if(_carID == request.query.ID || '' == request.query.ID){
+          let _car = [_carID, _carYear, _carBrand, _carModel, _carEngine]
+          row.push(_car)
+        }
+      }
+      //Return HTML
+      response.send(row);
+    })
+  })
+
+});
+
+//Server Directories.
 app.use(express.static('Cars'))
 app.use('/css',express.static(__dirname + '/css'))
 app.use('/js',express.static(__dirname + '/js'))
 app.use('/xml',express.static(__dirname + '/xml'))
 
+//Server Listener.
 app.listen(port, () => console.info('listening on port ' + port))
